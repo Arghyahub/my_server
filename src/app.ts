4721883bdd39d1d4435325318be7a183b25fb008
Arghya <arghyadas242004@gmail.com>
2024-07-10 21:37:34 +0530

    restrict cors

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000", "https://arghyas-portfolio.vercel.app/"],
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://arghyas-portfolio.vercel.app/"],
+  })
+);
 app.use(express.json());
 
 connectDB();
