340cb1c84a065a12b870f686c2dd22ae50b0aeb5
Arghya <arghyadas242004@gmail.com>
2024-07-11 01:04:33 +0530

    vercel redeploy

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,7 +5,7 @@ const DB_URL = process.env.MONGODB || "mongodb://localhost:27017/test";
 export default async function connectDB() {
   try {
     await mongoose.connect(DB_URL);
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB successfully!");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
   }
