5448aeec0b0fe0f65a5b6b3c793ee1be12ac6c2d
Arghya <arghyadas242004@gmail.com>
2024-07-11 01:22:55 +0530

    vercel didn't workout, moving to render

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 const DB_URL = process.env.MONGODB || "mongodb://localhost:27017/test";
 
-console.log(DB_URL);
-
 export default async function connectDB() {
   try {
     await mongoose.connect(DB_URL);
